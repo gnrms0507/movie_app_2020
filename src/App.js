d7ae11a5a779246678c08d70a3814a5c59cf377b
unknown <gnrms0507@email.daelim.ac.kr>
2020-09-18 12:12:50 +0900

    Edit readme.md

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+function Food({fav}){
+  return (
+  <h3>I like {fav}</h3>
+  )
+}
+
+
+
+
 function App() {
   //return <div className="App" />;
   return(
    <div>
      <h1>Hello</h1>
+     <Food fav="kimch" />
      </div>);
 }
 
