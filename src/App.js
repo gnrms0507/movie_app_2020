469a2cd5962c56933406a9e920600c289f168da6
unknown <gnrms0507@email.daelim.ac.kr>
2020-09-18 12:47:51 +0900

    201740215박희권

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function Food({fav}){
   )
 }
 
+const foodLike = [
+  {
+    name:'김치',
+  },
+  {
+    name:'만두',
+  },
+  {
+    name:'피자',
+  },
+];
+
 
 
 
@@ -14,13 +26,8 @@ function App() {
   return(
    <div>
      <h1>Hello</h1>
-     <Food fav="kimch" />
-     <Food fav="밥" />
-     <Food fav="칭따오" />
-     <Food fav="양꼬치" />
-     <Food fav="훠궈" />
-     <Food fav="마라탕" />
-     </div>);
+     </div>
+     );
 }
 
 export default App;
