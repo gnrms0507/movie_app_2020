90b322f21cc314934e892829efd4e59a08d63c64
unknown <gnrms0507@email.daelim.ac.kr>
2020-11-27 13:14:17 +0900

    201740215박희권1127실습

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,39 +1,38 @@
 import React from 'react';
-
+import './Detail.css';
 class Detail extends React.Component{
-    componentDidMount(){
-        const {location,history} = this.props;
-        if(location.state === undefined){
-            history.push('/');
-        }
+    componentDidMount() {
+        const { location, history }= this.props;
+
+      if(location.state === undefined){
+          history.push('/');
+      }  
     }
+
     render(){
-        const {location}=this.props;
-        if(location.state){
-            return( 
-                <div>
-                    <h1>{location.state.title}</h1>
-            <img src={location.state.poster} alt={location.state.title} title={location.state.title} height="500" width="500"/> 
-               <h4> {location.state.year}movie</h4>
-           <h3> {location.state.summary.slice(0, 100)}</h3>
-           <h3> {location.state.summary.slice(100, 300)}</h3>
-           <h3> {location.state.summary.slice(300, 500)}</h3>
-           <h2>{location.state.rank}/5.0</h2>
+const { location } = this.props;
+if(location.state){
+    return(
+        <div className="detail">
 
-            </div>
-
-
-            );
-        }else{
-            return null;
-
-
-
+            <h2>{location.state.title}    <hr /> rating  {location.state.rating}/10</h2>
+            <img src={location.state.poster} alt={location.state.title} title={location.state.title}  />
+            <h4> {location.state.year}movie</h4>
+           <h3><p className="movie-summary">{location.state.summary}</p> </h3> 
+   
+          
             
-        }
+        
+        </div>
+        
+        );
+}else{
+    return null;
+}
        
     }
 }
+
 
 
 export default Detail;
